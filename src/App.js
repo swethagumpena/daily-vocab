cf89c5bccca3693fa6826d09728e286b36c5a2ad
swetha <swetha.gumpena0607@gmail.com>
2021-03-13 06:35:54 +0530

    Create add word form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import {
 import './App.css';
 import Index from './components/Index/Index';
 import Home from './components/Home/Home';
+import AddWord from './components/AddWord/AddWord';
 
 function App() {
-  const [vocab] = useState({ eat: 'I am eating' });
-  // {word:sentence}
+  const [vocab] = useState([
+    { word: 'eat', sentence: 'I am eating' },
+    { word: 'eat', sentence: 'I am eating' },
+  ]);
+
+  const addNewVocab = (newVocab) => {
+    console.log(newVocab);
+  };
+
   return (
     <div className="App">
 
@@ -21,7 +29,10 @@ function App() {
             <Home vocab={vocab} />
           </Route>
           <Route path="/add-word">
-            {/* <AddWord /> */}
+            <AddWord
+              vocab={vocab}
+              addNewVocab={addNewVocab}
+            />
           </Route>
         </Switch>
       </BrowserRouter>
---
+++ src/components/AddWord/AddWord.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { useFormik } from 'formik';
+
+const AddWord = ({ vocab, addNewVocab }) => {
+  const [addWordForm, setAddWordForm] = useState({
+    addWordData: {
+      word: '',
+      sentence: '',
+    },
+    submitted: false,
+  });
+
+  const formik = useFormik({
+    initialValues: addWordForm.addWordData,
+    onSubmit: (addWordData) => setAddWordForm({ addWordData, submitted: true }),
+  });
+
+  return (
+    <div>
+      <h4>Add a new word</h4>
+      {!addWordForm.submitted ? (
+        <>
+          {/* <p>What's the word</p> */}
+          <form onSubmit={formik.handleSubmit}>
+            <label htmlFor="word">
+              Whats the word:
+              <input
+                name="word"
+                type="text"
+                placeholder="new word"
+                onChange={formik.handleChange}
+              />
+            </label>
+
+            <label htmlFor="sentence">
+              Lets use it in a sentence:
+              <input
+                name="sentence"
+                type="text"
+                placeholder="new sentence"
+                onChange={formik.handleChange}
+              />
+            </label>
+
+            <button type="submit">Tick</button>
+          </form>
+        </>
+      ) : (
+        <p>{`Your word ${addWordForm.addWordData.word} has been added`}</p>
+      )}
+    </div>
+  );
+};
+
+export default AddWord;
