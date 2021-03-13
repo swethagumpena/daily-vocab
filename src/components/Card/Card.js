76350bf2a8b53dce46bb55feb181f2c7e8715bde
swetha <swetha.gumpena0607@gmail.com>
2021-03-13 10:33:27 +0530

    Display words and sentences in Home page

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,17 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-const Card = ({ vocab }) => (
-  <>
-    <div>Card</div>
-    <div>{JSON.stringify(vocab)}</div>
-  </>
-);
+const Card = ({ word, sentence }) => {
+  const a = 10;
+  return (
+    <>
+      {/* <div>{JSON.stringify(vocab)}</div> */}
+      <p>{word}</p>
+      <p>{sentence}</p>
+      <hr />
+    </>
+  );
+};
 
 export default Card;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,13 +5,37 @@ import Card from '../Card/Card';
 
 const Home = ({ vocab }) => {
   const history = useHistory();
+  const singleVocab = vocab.map((item) => (
+    <Card
+      word={item.addWordData.word}
+      sentence={item.addWordData.sentence}
+    />
+  ));
   return (
     <>
       <div>Home</div>
-      <Card vocab={vocab} />
+      {singleVocab}
       <button type="button" onClick={() => history.push('/add-word')}>+</button>
     </>
   );
 };
+
+// vocab
+// [
+//   { addWordData: { word: 'cogent', sentence: 'belie elish' } },
+//   { addWordData: { word: 'cogent', sentence: 'belie elish' } },
+//   { addWordData: { word: 'belie', sentence: 'ghjkm' } },
+//   { addWordData: { word: 'abate', sentence: 'ertyu' } },
+//   { addWordData: { word: 'compilant', sentence: 'belie elish' } },
+// ];
+
+// singleVocab
+// [
+//     {"word":"cogent","sentence":"belie elish"},
+//     {"word":"cogent","sentence":"belie elish"},
+//     {"word":"belie","sentence":"ghjkm"},
+//     {"word":"abate","sentence":"ertyu"},
+//     {"word":"compilant","sentence":"belie elish"}
+// ]
 
 export default Home;
